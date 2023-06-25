78f52cc51a76958987eb0dbaab4c3f2e1d2e40c6
SantiagoFK <84585956+SantiagoFK@users.noreply.github.com>
2023-06-24 23:40:53 -0300

    Añadido carrusel imagenes en la pagina principal, algo de lorem tmb.

--- src/main/java/sistemaHotelUtn/GestionHotelApplication.java
+++ src/main/java/sistemaHotelUtn/GestionHotelApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionHotelApplication {
-
+public class GestionHotelApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(GestionHotelApplication.class, args);
 	}
-
 }
